80182e1890f5046147357769d87ee718fd91650d
Austin <ajtuwiner@gmail.com>
2018-11-08 11:03:02 -0600

    updates

---
+++ assets/main.js
@@ -0,0 +1,83 @@
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new employees - then update the html + update the database
+// 3. Create a way to retrieve employees from the employee database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDOR8iog9sW68qavqnhEDkj5HrM_bR4_vE",
+  authDomain: "pharmacydelivery-1ff2b.firebaseapp.com",
+  databaseURL: "https://pharmacydelivery-1ff2b.firebaseio.com",
+  projectId: "pharmacydelivery-1ff2b",
+  storageBucket: "pharmacydelivery-1ff2b.appspot.com",
+  messagingSenderId: "666352922440"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// 2. Button for adding Employees
+$("#add-employee-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var name = $("#name").val().trim();
+  var deliveryAddress = $("#delivery-address").val().trim();
+  var perscriptionNumber = moment($("#perscription-number").val().trim());
+  var pharmAddress = $("#pharmacy-address").val().trim();
+
+  // Creates local "temporary" object for holding employee data
+  var newEntry = {
+    name: name,
+    deliveryAddress: deliveryAddress,
+    perscriptionNumber: perscriptionNumber,
+    pharmAddress: pharmAddress
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newEmp);
+  // Logs everything to console
+  console.log(newEntry.name);
+  console.log(newEntry.deliveryAddress);
+  console.log(newEntry.perscriptionNumber);
+  console.log(newEntry.pharmAddress);
+
+  alert("Entry successfully added");
+  // Clears all of the text-boxes
+  $("#name").val("");
+  $("#deliveryAddress").val("");
+  $("#perscriptionNumber").val("");
+  $("#pharmAddress").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+  // Store everything into a variable.
+  var name = childSnapshot.val().name;
+  var deliveryAddress = childSnapshot.val().deliveryAddress;
+  var perscriptionNumber = childSnapshot.val().perscriptionNumber;
+  var pharmAddress = childSnapshot.val().pharmAddress;
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(name),
+    $("<td>").text(deliveryAddress),
+    $("<td>").text(perscriptionNumber),
+    $("<td>").text(pharmAddress),
+  );
+  // Append the new row to the table
+  $("#employee-table > tbody").append(newRow);
+});
+
+// Example Time Math
+// -----------------------------------------------------------------------------
+// Assume Employee start date of January 1, 2015
+// Assume current date is March 1, 2016
+
+// We know that this is 15 months.
+// Now we will create code in moment.js to confirm that any attempt we use meets this test case
